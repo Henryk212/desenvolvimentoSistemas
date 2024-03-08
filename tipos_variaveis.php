57d993cbf9b386048a4271306ca080df8ce54607
Auto Logon <autologon@CSED.LAB>
2024-03-08 19:56:03 -0300

    Aula desenvolvimento de sistemas

---
+++ tipos_variaveis.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 2</title>
+</head>
+
+<body>
+    <?php
+
+    echo "Olá Mundo !! <br>";
+
+    $varPrint = "Olá Mundo !! ";
+
+    echo $varPrint;
+
+    ?>
+
+</body>
+
+</html>
