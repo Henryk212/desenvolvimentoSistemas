b6722d7ea9c27f74fa2aa0b81f7d034f501b4f24
Henrique Onorato <glincon44@gmail.com>
2024-03-15 20:41:25 -0300

    Exercicios aula 3

---
+++ aula3/Estruturas_de_Controle.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estruturas de controle</title>
+    <!-- Desenvolva um código que receba a idade o peso de uma pessoa. Apresente ao usuario a idade, 
+    peso e o grupo de risco que a pessoa pertence(Desconsidere a altura da pessoa)
+    Apresentando o resultado na tela do navegador.
+
+    Idade < 18 anos e peso < 50,risco medio;
+    Idade < 18 anos e peso >= 50 e <= 90 risco NOrmal;
+    Idade < 18 anos e peso > 90  e <= 120 risco ALTO;
+    Idade < 18 anos e peso > 120 risco Extremamente alto;
+
+    Idade >= 18 e <= 50 e peso < 60 risco medio;
+    Idade >= 18 anos <= 50 e peso >= 60 e <= 100 risco NOrmal;
+    Idade >= 18 anos <= 50 e peso > 100 e <= 140 risco alto;
+    Idade >= 18 anos <= 50 e peso > 140 e <= 200 risco Extremamente alto; -->
+</head>
+<body>
+
+<?php
+$idade = 19;
+$peso = 500;
+$risco = "";
+
+if ($idade < 18) {
+    if ($peso < 50) {
+        $risco = "médio";
+    } elseif ($peso >= 50 && $peso <= 90) {
+        $risco = "normal";
+    } elseif ($peso > 90 && $peso <= 120) {
+        $risco = "alto";
+    } else {
+        $risco = "extremamente alto";
+    }
+} elseif ($idade >= 18 && $idade <= 50) {
+    if ($peso < 60) {
+        $risco = "médio";
+    } elseif ($peso >= 60 && $peso <= 100) {
+        $risco = "normal";
+    } elseif ($peso > 100 && $peso <= 140) {
+        $risco = "alto";
+    } else {
+        $risco = "extremamente alto";
+    }
+}
+
+echo "Idade: $idade<br>";
+echo "Peso: $peso<br>";
+echo "Risco: $risco<br>";
+?>
+
+<?php
+
+    $lado1 = 10;
+    $lado2 = 16;
+    $lado3 = 7;
+
+    if ($lado1 + $lado2 > $lado3 && $lado1 + $lado3 > $lado2 && $lado2 + $lado3 > $lado1) {
+        if ($lado1 == $lado2 && $lado2 == $lado3) {
+            echo "O triângulo é equilátero";
+        } elseif ($lado1 == $lado2 || $lado1 == $lado3 || $lado2 == $lado3) {
+            echo "O triângulo é isósceles";
+        } else {
+            echo "O triângulo é escaleno";
+        }
+    } else {
+        echo "Não é um triângulo";
+    }
+
+?>
+
+<?php
+
+    $numero = 15;
+    echo "<br> Verificando Numeros pares e impares<br>";
+
+    if($numero % 2 == 0 ){
+        echo "O numero $numero é par";
+    }else{
+        echo "O numero $numero é impar";
+    }
+
+?>
+<?php
+
+    echo "<br>Ano Eleitoral<br>";
+
+    $anoNascimento = 2023;
+    $anoAtual = date('Y');
+
+    $anoAtualConvertido = (int) $anoAtual;
+
+    $idade = $anoAtualConvertido - $anoNascimento;
+
+    if(($idade) >= 18 ){
+        echo "Você tem $idade Você pode votar !!";
+    }else{
+        echo "Você tem $idade Você Não pode votar !!";
+    }
+
+?>
+<?php
+
+    echo "<br>Somas <br>";
+
+$numero = 10;
+$operacao = 'dobro';
+$resultado = 0;
+
+switch ($operacao) {
+    case 'dobro':
+        $resultado = $numero * 2;
+        break;
+    case 'cubo':
+        $resultado = pow($numero, 3);
+        break;
+    case 'raiz':
+        $resultado = sqrt($numero);
+        break;
+    default:
+        echo "Operação não reconhecida";
+        exit();
+}
+
+echo "O resultado da operação $operacao no número $numero é: $resultado";
+
+
+$resultado = match ($operacao) {
+    'dobro' => $numero * 2,
+    'cubo' => pow($numero, 3),
+    'raiz' => sqrt($numero),
+    default => exit("Operação não reconhecida"),
+};
+
+echo "O resultado da operação $operacao no número $numero é: $resultado";
+?>
+?>
+
+
+
+
+
+    
+</body>
+</html>
